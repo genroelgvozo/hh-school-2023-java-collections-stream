fc0fa6c8190d1f4c0ca1ff9c3c3860c8e3c60227
Sergei Boriskin <bars.9288@gmail.com>
2023-11-27 20:20:30 +0300

    3 ready

--- src/main/java/tasks/Task3.java
+++ src/main/java/tasks/Task3.java
@@ -1,9 +1,9 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
 Задача 3
@@ -12,6 +12,22 @@ import java.util.List;
 public class Task3 {
 
   public static List<Person> sort(Collection<Person> persons) {
-    return new ArrayList<>(persons);
+    ArrayList<Person> sortedList = new ArrayList<>();
+
+              persons.stream()
+                      .sorted(Comparator.comparing(Person::getFirstName)
+                    .thenComparing(Person::getSecondName)
+                    .thenComparing(Person::getCreatedAt))
+                      .forEach(x -> sortedList.add(x));
+
+    return sortedList;
+
+      // сложность по времени - O(n * logN * logN) ( logN - добавление элементов в сортированную таблицу по величине CreatedAt и getSecondName и для каждого getFirstName которых n)
+      // сложность по памяти - O(n) ( поле ЗНАЧЕНИЕ в списке )
+
+    // не потерять - мануал откуда брал
+    // https://user12vv.wordpress.com/2017/06/05/%D1%81%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2-%D0%B2-java8-comparator-comparing-reversed/
+
+
   }
 }
