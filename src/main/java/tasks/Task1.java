7461599b24f37564e9d436bbe09e9f0500f4f9ec
ChillyApril <sashaokho@gmail.com>
2023-12-05 00:53:01 +0500

    Task1 is solved.

--- src/main/java/tasks/Task1.java
+++ src/main/java/tasks/Task1.java
@@ -3,26 +3,47 @@ package tasks;
 import common.Person;
 import common.PersonService;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.Map;
+import java.util.ArrayList;
 
 /*
-Задача 1
-Метод на входе принимает List<Integer> id людей, ходит за ними в сервис
-(он выдает несортированный Set<Person>, внутренняя работа сервиса неизвестна)
-нужно их отсортировать в том же порядке, что и переданные id.
-Оценить асимпотику работы
+асимптотика:
+  -создание словаря O(n)
+  -заполнение массива O(n)
+Итог: O(n)
  */
 public class Task1 {
 
   private final PersonService personService;
 
   public Task1(PersonService personService) {
+    
     this.personService = personService;
   }
 
   public List<Person> findOrderedPersons(List<Integer> personIds) {
+
     Set<Person> persons = personService.findPersons(personIds);
-    return Collections.emptyList();
+
+    List<Person> sorted_persons = new ArrayList<>(personIds.size());
+
+    Map<Integer, Person> id_person = new HashMap<>();
+
+    for (Person person : persons){
+
+        id_person.put(person.getId(), person);
+
+    }
+    for (Integer id : personIds){
+
+        sorted_persons.add(id_person.get(id));
+
+    }
+
+    return sorted_persons;
   }
 }
+
