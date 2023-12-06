d908599250ab8dce7f63f05f13c0ef8da27439a7
ChillyApril <sashaokho@gmail.com>
2023-12-06 18:52:28 +0500

    Task2 is solved.

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -3,19 +3,24 @@ package tasks;
 import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Collections;
 
 /*
-Задача 2
-На вход принимаются две коллекции объектов Person и величина limit
-Необходимо объеденить обе коллекции
-отсортировать персоны по дате создания и выдать первые limit штук.
+Асимптотика: O( n * ln(n) )
  */
 public class Task2 {
-
+  
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    return new ArrayList<>();
+    Integer n = persons1.size() + persons2.size();
+    List<Person> merged_people = new ArrayList<>(n);
+    merged_people.addAll(persons1);
+    merged_people.addAll(persons2);
+    Comparator<Person> time =(person1, person2) -> person1.getCreatedAt().compareTo(person2.getCreatedAt());
+    Collections.sort(merged_people, time);
+    return merged_people.subList(0, Math.min(limit, n));
   }
 }
